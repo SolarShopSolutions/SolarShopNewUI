a27e7c4d57b016be24660a03b05c8dabda9bad95
Mayank Sati <mmayanksati@gmail.com>
2024-05-04 18:07:36 +0530

    all files

---
+++ src/components/scroll.jsx
@@ -0,0 +1,369 @@
+import WhyUs1 from "../common/images/whyUsDecision-making_2620308.png";
+import WhyUs2 from "../common/images/whyUsMaintenance_769198.png";
+import WhyUs3 from "../common/images/whyUsSolar-panels_4114997.png";
+import WhyUs4 from "../common/images/whyUsThumbs-up_6043309.png";
+import WhyUs5 from "../common/images/whyUsTracking_13386211.png";
+import WhyUs6 from "../common/images/whyUsWarranty_950264.png";
+
+export default function scroll() {
+  return (
+    <>
+      <section className="clients-section" style={{ padding: "20px" }}>
+        <div className="auto-container">
+          <div className="sec-title text-center">
+            <h2 className="pt-4">Our Valuable Clients</h2>
+            <div className="separator">
+              <i className="fa fa-globe" aria-hidden="true"></i>
+            </div>
+          </div>
+          <marquee
+            scrollamount="15"
+            onmouseover="this.stop()"
+            onmouseout="this.start()"
+            // onMouseOver={this.stop()}
+            // onMouseOut={this.start()}
+          >
+            <ul className="clts">
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs1}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs2}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs3}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs4}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs5}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs6}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs1}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs2}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs3}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs4}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs5}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs6}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs1}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs2}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs3}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs4}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs5}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs6}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs1}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs2}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs3}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs4}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs5}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+              <li
+                className="mr-2"
+                style={{
+                  boxShadow: "0 0 10px rgba(0,0,0,.3)",
+                  marginRight: "20px",
+                }}
+              >
+                <img
+                  src={WhyUs6}
+                  width="300"
+                  height="200"
+                  style={{ padding: "2px", border: "2px solid black" }}
+                />
+              </li>
+            </ul>
+          </marquee>
+        </div>
+      </section>
+    </>
+  );
+}
